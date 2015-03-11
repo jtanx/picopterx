1aec2d7cda0f729d6bd2849d2b99dab5ff7b35c1
Jeremy Tan <jtanx@outlook.com>
2015-03-11 20:12:07 +0800

    Add in codes for gpsd emu

--- code/emulation/gps-emu/libgpsmm.h
+++ code/emulation/gps-emu/libgpsmm.h
@@ -22,7 +22,39 @@
 #define WATCH_ENABLE 1
 #define WATCH_JSON 1
 
-#define LATLON_SET 1
+#define ONLINE_SET	(1llu<<1)
+#define TIME_SET	(1llu<<2)
+#define TIMERR_SET	(1llu<<3)
+#define LATLON_SET	(1llu<<4)
+#define ALTITUDE_SET	(1llu<<5)
+#define SPEED_SET	(1llu<<6)
+#define TRACK_SET	(1llu<<7)
+#define CLIMB_SET	(1llu<<8)
+#define STATUS_SET	(1llu<<9)
+#define MODE_SET	(1llu<<10)
+#define DOP_SET  	(1llu<<11)
+#define HERR_SET	(1llu<<12)
+#define VERR_SET	(1llu<<13)
+#define ATTITUDE_SET	(1llu<<14)
+#define SATELLITE_SET	(1llu<<15)
+#define SPEEDERR_SET	(1llu<<16)
+#define TRACKERR_SET	(1llu<<17)
+#define CLIMBERR_SET	(1llu<<18)
+#define DEVICE_SET	(1llu<<19)
+#define DEVICELIST_SET	(1llu<<20)
+#define DEVICEID_SET	(1llu<<21)
+#define RTCM2_SET	(1llu<<22)
+#define RTCM3_SET	(1llu<<23)
+#define AIS_SET 	(1llu<<24)
+#define PACKET_SET	(1llu<<25)
+#define SUBFRAME_SET	(1llu<<26)
+#define GST_SET 	(1llu<<27)
+#define VERSION_SET	(1llu<<28)
+#define POLICY_SET	(1llu<<29)
+#define LOGMESSAGE_SET	(1llu<<30)
+#define ERROR_SET	(1llu<<31)
+#define TIMEDRIFT_SET	(1llu<<32)
+#define EOF_SET		(1llu<<33)
 
 struct gps_data_t {
     struct fix {
@@ -31,6 +63,10 @@ struct gps_data_t {
         double epy;
         double longitude;
         double epx;
+        double speed;
+        double eps;
+        double track;
+        double epd;
     } fix;
     int set;
 };
