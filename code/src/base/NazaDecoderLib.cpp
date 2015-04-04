2845b473b5bc2c639c92f8e99ecd005a070a953c
BrettRD <brett.downing1@gmail.com>
2015-04-04 12:33:30 +0800

    fixed compass Z masking

--- code/src/base/NazaDecoderLib.cpp
+++ code/src/base/NazaDecoderLib.cpp
@@ -133,7 +133,7 @@ uint8_t NazaDecoderLib::decode(int input)
             int16_t y = decodeShort(2, mask);
 
             //three axis compass according to http://www.rcgroups.com/forums/showpost.php?p=26248426&postcount=62
-            int16_t z = (payload[5] << 8) | (payload[4] ^ mask);  //can't use decodeShort() because byte 9 is not XORed
+            int16_t z = ((payload[5] ^ mask) << 8) | (payload[4]);  //can't use decodeShort() because byte 9 is not XORed
 
             //this makes sense, but it's pretty crude. The copter will need to do a full rotation before it finds the centres of its magnetometers
             //if we want to be really clever, we should probably store these values somwhere.
