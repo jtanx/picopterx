8df9b04d81fabbb7750990c21d9b0d9a3d29d465
Jeremy Tan <jtanx@outlook.com>
2015-07-30 14:29:21 +0800

    +x mjpg-streamer.sh

--- scripts/mjpg-streamer.sh
+++ scripts/mjpg-streamer.sh
@@ -3,7 +3,7 @@
 export LD_LIBRARY_PATH=/usr/local/lib
 
 pkill mjpg_streamer
-v4l2-ctl --set-fmt-video=width=320,height=240
-v4l2-ctl --set-parm 30
+#v4l2-ctl --set-fmt-video=width=320,height=240
+#v4l2-ctl --set-parm 30
 mjpg_streamer -i 'input_file.so -f /mnt/ramdisk' &
 echo ' '
