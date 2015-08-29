242e73bebaa7b9b145bb2cc526c8d809f4fd068e
BrettRD <brett.downing1@gmail.com>
2015-08-30 01:14:41 +0800

    Changed to matrices, wrote base transformations

--- code/include/observations.h
+++ code/include/observations.h
@@ -3,19 +3,40 @@
  * @brief The header file for the observations code.
  */
 
+
+/*
+    a 3d gaussian elliptical structure: (treat as a probability density function PDF)
+        p = e^-(ax^2 + by^2 + cz^2 + dyz + ezx + fxy + g)
+    The semi-major axes are encoded with
+        a,b,c, and d,e,f
+    the centre of the ellipse the stationary point of the polynomial
+        ax^2 + by^2 + cz^2 + dyz + ezx + fxy + g
+    combining independent measurements of an object location is dine by multiplying PDFs, or in this case, summing polynomials.
+
+    For the sake of sanity and legibility, converting to matrix definition.
+        This increases the complexity of combining observations, but not by much.
+        It also keeps the transforation matrices at 3x3 instead of 10x10 so the opencv API is friendlier
+
+*/
+
+
 #ifndef _PICOPTERX_OBSERVATIONS_H
 #define _PICOPTERX_OBSERVATIONS_H
 
 #include "opts.h"
 #include "flightcontroller.h"
 #include "camera_stream.h"
+#include <opencv2/opencv.hpp>
 
 namespace picopter {
 
     //a 3d gaussian elliptical structure
+
     const int _distrib_coeffs = 10;
     typedef struct Distrib {
-        double coeffs[10];
+        cv::Matx33d axes;   //eigenvectors are the semi-major axes, eigenvalues are the sigma=1 widths
+        cv::Matx31d location;   //offset from origin column vector
+        //double coeffs[10];
     } Distrib;
 
     //parameters to construct the elliptic structure above
@@ -25,20 +46,30 @@ namespace picopter {
         double yaw, pitch, roll;
     } DistribParams;
 
-
     //where the observation came from
     typedef enum Source {   //should probably be a collection of booleans instead of splitting sensor values
-        CAMERA_BLOB,
-        CAMERA_SIFT,
-        LIDAR,
-        TELEM
+        CAMERA_BLOB,    //blob detection has no sense of range, (include a picture)
+        CAMERA_SIFT,    //SIFT doesn't resolve range (include a picture, and the SIFT characteristic data)
+        CAMERA_FLOW,    //optical flow will resolve a range and blob (probably include two images and IMU velocity)
+        LIDAR,          //lidar sensor has a small dot, but can't identify objects
+        FLOW,           //flow sensor has a large width uncertainty
+        TELEM           //objects transmitted from external sources
     } Source;
     //ObjectInfo    //detections from the camera stream (grouped into single objects)
 
     typedef struct Observation {
-        Distrib distrib;    //3D probability density function (elliptical normal)
-        Source source;              //sensor pack the detection came from
-        ObjectInfo* camDetection;   //information from camera detection
+        //location of copter +uncertainty
+        //time
+        //IMU data (including velocity)
+        //3D probability density function (elliptical normal)
+        Distrib distrib;
+        //sensor pack the detection came from
+        Source source;
+
+        //information from camera detection defined in camera_stream
+        ObjectInfo* camDetection;
+        //something for lidar data
+        //something for
 
     } Observation;
 
--- code/src/modules/observations.cpp
+++ code/src/modules/observations.cpp
@@ -10,13 +10,13 @@
 #include "observations.h"
 
 using namespace picopter;
-
+using namespace cv;
 Observations::Observations() {
 
 }
 
 
-//estimate the probability the given observation  is of the same object
+//estimate the probability the given observation is of the same object
 double getSameProbability(Observation* observation){    
 
 return 0;
@@ -24,37 +24,43 @@ return 0;
 
 //calculate the centre and covariance widths of this object
 DistribParams getDistribParams(){                         
+
     DistribParams D = {0};
     return D;
-
 }
 
 //add another sighting to this object
 void Observations::appendObservation(Observation* observation){     
     sightings.push_back(observation);
-    for(int i=0; i<_distrib_coeffs; i++){
-        distrib.coeffs[i] += observation->distrib.coeffs[i];
-    }
-
+    //for(int i=0; i<_distrib_coeffs; i++){
+    //    distrib.coeffs[i] += observation->distrib.coeffs[i];
+    //}
 }
 
 //remove an observation from this object
 void Observations::removeObservation(Observation* observation){     
-
+    //riffle through the sightings until we find an identical pointer to this observation?
+        //remove it, replace with the tail?
+        //sightings.at(n) = pop_back(); //fast, breaks order
+        //sightings.erase(n);   //slow, preserves order
 
 }
 
 //generate an distrib struct from a primitive and operators
 Distrib generateDistrib(DistribParams params){
     //primitive is a, spherical distribution sigma=1
-    Distrib primitive;
-    for (int i = 0; i < 3; i++) {
-        primitive.coeffs[i] = 0.5;
-    }
-    for (int i = 3; i < _distrib_coeffs; i++) {
-        primitive.coeffs[i] = 0;
-    }
-    
+    Matx33d axes ( 
+        0.5, 0, 0,
+        0, 0.5, 0,
+        0, 0, 0.5);
+    Matx31d location (0,0,0);
+    Distrib primitive = {axes,location};// = {0.5,0.5,0.5, 0,0,0, 0,0,0, 0};
+//    for (int i = 0; i < 3; i++) {
+//        primitive.coeffs[i] = 0.5;
+//    }
+//    for (int i = 3; i < _distrib_coeffs; i++) {
+//        primitive.coeffs[i] = 0;
+//    }
     Distrib dS = stretchDistrib(primitive, params.sigma_x, params.sigma_y, params.sigma_z);
     Distrib dR = rotateDistrib(dS, params.yaw, params.pitch, params.roll);
     Distrib dT = translateDistrib(dR, params.x, params.y, params.z);
@@ -63,30 +69,71 @@ Distrib generateDistrib(DistribParams params){
 }
 
 
-DistribParams getDistribParams(Distrib A);                  //calculate the centre and covariance widths of this object
+//calculate the centre and covariance widths of this object
+DistribParams getDistribParams(Distrib A){
+    DistribParams D;
+    //sigma values are the eigenvalues
+    D.x = -A.location(0,0);
+    D.y = -A.location(1,0);
+    D.y = -A.location(2,0);
+    return D;
+}
 
 //combine two distributions (as though statistically independent)
 Distrib combineDistribs(Distrib A, Distrib B){
     Distrib C;
-    for(int i=0; i<_distrib_coeffs; i++){
-        C.coeffs[i] = A.coeffs[i] + B.coeffs[i];
-    }
+    //so easy in polynomial form, much harder in vector form
+    //for(int i=0; i<_distrib_coeffs; i++){
+    //    C.coeffs[i] = A.coeffs[i] + B.coeffs[i];
+    //}
     return C;
 }
 
 //translate a distrib struct from the origin
 Distrib translateDistrib(Distrib A, double x, double y, double z){
-    Distrib D = {0};
+    cv::Matx31d offset(x,y,z);
+    Distrib D;
+    D.axes = A.axes;
+    D.location = A.location - offset;
     return D;
 }
 
 //translate a distrib struct about the origin
 Distrib rotateDistrib(Distrib A, double yaw, double pitch, double roll){
-    Distrib D = {0};
+    Distrib D;
+    double a;
+    a = pitch;
+    Matx33d Rx(
+        1,      0,       0,
+        0, cos(a), -sin(a),
+        0, sin(a),  cos(a));
+    a = roll;
+    Matx33d Ry(
+         cos(a), 0, sin(a),
+              0, 1,      0,
+        -sin(a), 0, cos(a));
+    a = yaw;
+    Matx33d Rz(
+        cos(a), -sin(a), 0 , 
+        sin(a),  cos(a), 0 , 
+             0,       0, 1 ); 
+
+
+    D.axes = Rx.inv() * Ry.inv() * Rz.inv() * A.axes * Rz * Ry * Rx;    //I think
+    D.location = A.location;
     return D;
 }
-//stretch a distrib struct about the origin
+//stretch a distrib struct about the origin, only along the 
 Distrib stretchDistrib(Distrib A, double sx, double sy, double sz){
-    Distrib D = {0};
+    Distrib D;
+
+    Matx33d S(
+        sx, 0, 0,
+        0, sy, 0,
+        0, 0, sz);
+
+    D.location = S * A.location;    //increase distance from origin
+    D.axes = S.inv() * S.inv() * A.axes;    //increase the size
+
     return D;
 }
