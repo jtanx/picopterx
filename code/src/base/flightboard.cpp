c7380cd2be9fac2d4a4bf0216088b78aba675beb
BrettRD <brett.downing1@gmail.com>
2015-10-05 12:55:06 +0800

    briefly experimented with mixed messages

--- code/src/base/flightboard.cpp
+++ code/src/base/flightboard.cpp
@@ -599,9 +599,12 @@ bool FlightBoard::SetYaw(int bearing, bool relative) {
 bool FlightBoard::SetGimbalPose(EulerAngle pose) {
     mavlink_message_t msg;
     mavlink_mount_control_t gimbal = {};
+    
     //gimbal.command = MAV_CMD_DO_MOUNT_CONTROL;
+    
     gimbal.target_system = m_system_id;
     gimbal.target_component = m_component_id;
+    
     gimbal.input_a = pose.pitch*100; //pitch in deg
     gimbal.input_b = pose.roll*100; //roll
     gimbal.input_c = pose.yaw*100;   //yaw
@@ -621,16 +624,24 @@ bool FlightBoard::SetGimbalPose(EulerAngle pose) {
 bool FlightBoard::ConfigureGimbal() {
     mavlink_message_t msg;
     mavlink_mount_configure_t gimbal = {};
-    //gimbal.command = MAVLINK_MSG_ID_MOUNT_CONFIGURE;
+    //mavlink_command_long_t gimbal = {};
+
     gimbal.target_system = m_system_id;
     gimbal.target_component = m_component_id;
+    
+    //gimbal.command = MAV_CMD_DO_MOUNT_CONFIGURE;
+    //gimbal.param1 = MAV_MOUNT_MODE_MAVLINK_TARGETING;
+    //gimbal.param2 = 0;
+    //gimbal.param3 = 0;
+    //gimbal.param4 = 0;
+    //mavlink_msg_command_long_encode(m_system_id, m_flightboard_id, &msg, &gimbal);
+
     gimbal.mount_mode = MAV_MOUNT_MODE_MAVLINK_TARGETING; //pitch in deg
     gimbal.stab_roll = 0; //don't stabilize
     gimbal.stab_pitch = 0; //don't stabilize
     gimbal.stab_yaw = 0; //don't stabilize
-   
     mavlink_msg_mount_configure_encode(m_system_id, m_flightboard_id, &msg, &gimbal);
-    //mavlink_msg_command_long_encode(m_system_id, m_flightboard_id, &msg, &gimbal);
+   
     m_link->WriteMessage(&msg);
     return true;
 }
