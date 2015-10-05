30ea4e9c7082fba5b48cc9f62289e6e22107a39a
Jeremy Tan <jtanx@outlook.com>
2015-10-05 18:12:35 +0800

    Add in Manish's env mapping.

--- code/src/modules/env_mapping.cpp
+++ code/src/modules/env_mapping.cpp
@@ -4,8 +4,24 @@
 * Set an initial radius.
 */
 
+/*
+ Final functions structure:
+ 
+ ->>>> After pressing the env mapping.
+ Actions
+ (1) Lift of the GND (3 meters)
+ (2) Rotate to locate object (colour set by user)
+ (3) When object found, stop rotating and move towards the object (at a slow speed).
+ (4) Stop when the object is fiting the whole screen
+ (5) Rotate aroud the POI whilst taking pic and increaisng alt. (Height increment after completing every loop.
+ (6) If the object lost return to action (2)
+ 
+ End after completing the loops set by user
+ */
+
 #include "common.h"
 #include "env_mapping.h"
+#include "utility.h"
 #define _USE_MATH_DEFINES
 #include <cmath>
 
@@ -56,49 +72,86 @@ void EnvironmentalMapping::Run(FlightController *fc, void *opts) {
         return;
     }
 
+    //Takeoff if we're not in the air
+    if (!fc->fb->IsInAir()) {
+        UtilityModule module(UtilityModule::UTILITY_TAKEOFF);
+        module.Run(fc, reinterpret_cast<void*>(5));
+    }
+    
     SetCurrentState(fc, STATE_ENV_MAPPING);
     std::vector<ObjectInfo> objects;
-    //double start_radius = fc->lidar->GetLatest(); 
-    //double alt  = fc->gps->GetLatestRelAlt();
-    GPSData d;
-    fc->gps->GetLatest(&d);
-    Coord3D centre = {d.fix.lat, d.fix.lon, d.fix.alt - d.fix.groundalt};
-    //Vec3D offset = RotateBodyToNED(Vec3D{-5, 0, 0}, fc->imu->GetLatestYaw()); //5m to left of copter.
-    centre = CoordAddOffset(centre, Vec3D{-10, 0, 0}); //5m To west of copter.
-
-    fc->fb->SetRegionOfInterest(centre);
-    for (int j=0; j<3; j+=1) {
-        for (int i=0; i<360 && !fc->CheckForStop(); i=i+5) {
-            Coord3D location = CoordAddOffset(centre, 10/*start_radius*/, i);
-            GotoLocation(fc, location, centre, false);
-            Log(LOG_DEBUG, "%d", i);
-            
-            fc->cam->GetDetectedObjects(&objects);
-            if (objects.size() > 0) {
-                //Log(LOG_DEBUG, "%.7f, %.7f", centre.lat, centre.lon);    
-                std::string path = std::string(PICOPTER_HOME_LOCATION "/pics/mappingimages") +"_" +
-                std::to_string(location.lat) + "_"+ 
-                std::to_string(location.lon) + "_" + 
-                std::to_string(location.alt) +
-                std::string(".jpg");
-
-                fc->cam->TakePhoto(path);
-            }
-            fc->Sleep(100);        
+    
+    //Rotate to search
+    while (!fc->CheckForStop()) {
+        fc->fb->SetYaw(5, true);
+        fc->Sleep(100);
+        
+        fc->cam->GetDetectedObjects(&objects);
+        if (objects.size() > 0) {
+            //We have a detected object
+            break;
         }
-        centre.alt += j;
-        Log(LOG_DEBUG, "NEW LOOP");
     }
+    
+    //Move forward slowly
+    Vec3D msp{0, 0.2, 0};
+    while (!fc->CheckForStop()) {
+        fc->fb->SetBodyVel(msp);
+        fc->cam->GetDetectedObjects(&objects);
+        if (objects.size() > 0) {
+            ObjectInfo o = objects.front();
+            if (o.bounds.area() > 0.3*(o.image_width * o.image_height)) {
+                fc->fb->Stop();
+                break;
+                break;
+            }
+        }
+    }
+    
+    //while (!fc->CheckForStop()){
+    //    o.bounds.area > 0.1*(o.image_width * o.image_height))
+        
+    
+        //double start_radius = fc->lidar->GetLatest();
+        //double alt  = fc->gps->GetLatestRelAlt();
+        GPSData d;
+        fc->gps->GetLatest(&d);
+        Coord3D centre = {d.fix.lat, d.fix.lon, d.fix.alt - d.fix.groundalt};
+        centre = CoordAddOffset(centre, 10, fc->imu->GetLatestYaw()); //10m in front of copter
+        //centre = CoordAddOffset(centre, Vec3D{-10, 0, 0}); //5m To west of copter.
+
+        fc->fb->SetRegionOfInterest(centre);
+        for (int j=0; j<3; j+=1) {
+            for (int i=0; i<360 && !fc->CheckForStop(); i=i+5) {
+                Coord3D location = CoordAddOffset(centre, 10/*start_radius*/, i);
+                GotoLocation(fc, location, centre, false);
+                Log(LOG_DEBUG, "%d", i);
+                
+                fc->cam->GetDetectedObjects(&objects);
+                if (objects.size() > 0) {
+                    //Log(LOG_DEBUG, "%.7f, %.7f", centre.lat, centre.lon);
+                    std::string path = std::string(PICOPTER_HOME_LOCATION "/pics/mappingimages") +"_" +
+                    std::to_string(location.lat) + "_"+
+                    std::to_string(location.lon) + "_" +
+                    std::to_string(location.alt) +
+                    std::string(".jpg");
+
+                    fc->cam->TakePhoto(path);
+                }
+                fc->Sleep(100);
+            }
+            centre.alt += j;
+            Log(LOG_DEBUG, "NEW LOOP");
+        }
+    //}
 
     fc->fb->UnsetRegionOfInterest();
     fc->fb->Stop();
     m_finished = true;
 }
 
+    
 bool EnvironmentalMapping::Finished() {
     return m_finished;
 }
-
-
-
 
